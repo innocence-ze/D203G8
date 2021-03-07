712836a3d62c57ef46eb46b439ada2188e4f9290
zhaozeshi <944966330@qq.com>
2021-03-07 23:21:46 +0000

    checkpoint, enemy alpha version

---
+++ Assets/Scripts/Character/Enemy/RangedEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO,使用对象池优化
+public class RangedEnemy : NpcEnemy
+{
+    public GameObject Bullet;
+    public Transform shotPos;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(PatrolState());
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void InAttack()
+    {
+        Instantiate(Bullet, shotPos.position, Quaternion.identity);
+        Bullet.GetComponent<EnemyBullet>().direction =  (target.position - transform.position).normalized;
+    }
+}
