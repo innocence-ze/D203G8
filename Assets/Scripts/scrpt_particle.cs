c06b6bfc3f4043791b07f5078f1be40fd25aa6bf
Precotonejde <samuel.chaba82@gmail.com>
2021-03-18 19:55:01 +0100

    floating particles start

---
+++ Assets/Scripts/scrpt_particle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrpt_particle : MonoBehaviour
+{
+    Vector3 direction;
+    float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+        direction = direction.normalized;
+        speed = Random.Range(0.01f, 0.25f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
