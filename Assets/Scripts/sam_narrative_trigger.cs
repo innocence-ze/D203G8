f3e070a136f9116ad8f0cbe87a42539db4acb902
Precotonejde <samuel.chaba82@gmail.com>
2021-03-28 21:00:18 +0200

    checkpoints and narrative

---
+++ Assets/Scripts/sam_narrative_trigger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class sam_narrative_trigger : MonoBehaviour
+{
+    public int narrativeIndex;
+    private string docName = "subtitles.txt";
+    private string[] subtitleLines;
+
+    public Text mySubtitles;
+    public GameObject myTextBox;
+
+    private bool alreadyShown = false;
+
+    private void Awake()
+    {
+        myTextBox.SetActive(false);
+    }
+
+    void Start()
+    {
+        StreamReader sr = new StreamReader(Application.dataPath + "/" + docName);
+        string docContents = sr.ReadToEnd();
+        sr.Close();
+
+        subtitleLines = docContents.Split("\n"[0]);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            if (!alreadyShown)
+            {
+                alreadyShown = true;
+                mySubtitles.text = subtitleLines[narrativeIndex];
+                myTextBox.SetActive(true);
+            }
+        }
+    }
+}
