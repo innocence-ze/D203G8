638bd8d639288ca32e6805f2c57faa7347ef0375
ScottishArgonian <70216325+ScottishArgonian@users.noreply.github.com>
2021-04-05 12:58:35 +0100

    Update 04/03/21

---
+++ Assets/Scripts/Derek_narrative.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class Derek_narrative : MonoBehaviour
+{
+    public int narrativeIndex;
+    private string docName = "DerekSubtitles.txt";
+    private string[] subtitleLines;
+
+    public Text mySubtitles;
+    public GameObject myTextBox;
+
+    private bool alreadyShown = false;
+
+    private void Awake()
+    {
+        myTextBox.SetActive(false);
+    }
+
+    void Start()
+    {
+        StreamReader sr = new StreamReader(Application.dataPath + "/" + docName);
+        string docContents = sr.ReadToEnd();
+        sr.Close();
+
+        subtitleLines = docContents.Split("\n"[0]);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Debug.Log("Trigger Activated");
+
+            if (!alreadyShown)
+            {
+                alreadyShown = true;
+                mySubtitles.text = subtitleLines[narrativeIndex];
+                myTextBox.SetActive(true);
+            }
+        }
+    }
+}
